9759fa6d3ecd02193fce1ae3f6e90a27ce44fe07
Quim <quim@airmail.cc>
2018-03-12 17:51:11 +0100

    Improve Debugger class
    * Add backtrace
    * Add continue until {call,unknownCall,ret}
    
    d = r.debugger
    d.start()
     Continue until ret
    d.untilRet().cont()
    
    * Add easy way to write to registers
    
    d.CPU.rax = 0x100

--- python/r2api/debugger.py
+++ python/r2api/debugger.py
@@ -11,9 +11,27 @@ class CPU(R2Base):
 		except:
 			return None
 
-	def __getattr__(self, attr):
-		return self.readRegister(attr)
+	def writeRegister(self, reg_name, value):
+		res = self._exec('dr %s=%s' % (reg_name, value))
+		if res == '':
+			raise ValueError('Ivalid register %s' % reg_name)
 
+	def registers(self):
+		return self._exec('drj', json=True).keys()
+
+	def __getattr__(self, attr):
+		if attr in self.registers():
+			return self.readRegister(attr)
+
+	def __setattr__(self, attr, value):
+		if attr == 'r2':
+			# Hack to avoid infite recursion, maybe there's a better solution
+			self.__dict__[attr] = value
+		else:
+			if attr in self.registers():
+				self.writeRegister(attr, value)
+			else:
+				self.__dict__[attr] = value
 
 class Debugger(R2Base):
 	def __init__(self, r2):
@@ -21,15 +39,42 @@ class Debugger(R2Base):
 
 		self.CPU = CPU(r2)
 
+		self._untilCall = False
+		self._untilUnknownCall = False
+		self._untilRet = False
+
 		self.listBreakpoints = lambda: ResultArray(self._exec('dbj', json=True))
 		self.step = lambda: self._exec('ds')
 		self.memoryMaps = lambda: ResultArray(self._exec('dmj', json=True))
+		self.backtrace = lambda: self._exec('dbtj', json=True)
 
 	def start(self):
 		self._exec('doo')
 
 	def cont(self):
-		self._exec('dc')
+		if self._untilCall:
+			self._exec('dcc')
+		elif self._untilUnknownCall:
+			self._exec('dccu')
+		elif self._untilRet:
+			self._exec('dcr')
+		else:
+			self._exec('dc')
+		self._untilCall = False
+		self._untilRet = False
+		self._untilUnknownCall = False
+
+	def untilCall(self):
+		self._untilCall = True
+		return self
+
+	def untilRet(self):
+		self._untilRet = True
+		return self
+
+	def untilUnknownCall(self):
+		self._untilUnknownCall = True
+		return self
 
 	def setBreakpoint(self, addr=0):
 		if self._tmp_off != '':
