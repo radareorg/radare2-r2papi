4aa82513242fe2644959541a319806c9f1363d69
Quim <quim@airmail.cc>
2018-04-21 00:33:10 +0200

    First implementation of Print object
    Example:
        from r2api import R2Api
        r = R2Api('/bin/ls')
        # python3 or python2 with print_function
        r.print.at('entry0').bytes(10)
        r.print.at('entry0').disassemble(2)
        # python2
        r._print.at('entry0').bytes(10)
        r._print.at('entry0').disassemble(2)

---
+++ python/r2api/print.py
@@ -0,0 +1,32 @@
+from .base import R2Base
+
+class Print(R2Base):
+	def __init__(self, r2):
+		super(Print, self).__init__(r2)
+		self.hash_types = self._exec('ph').split()
+
+	def byte(self):
+		return self.bytes(1)[0]
+
+	def bytes(self, size):
+		ret = self._exec('p8j %s%s' % (size, self._tmp_off), json=True)
+		self._tmp_off = ''
+		return ret
+
+	def disassemble(self, size=0):
+		ret = self._exec('pdj %s%s' % ('' if size == 0 else size,
+									   self._tmp_off), json=True)
+		self._tmp_off = ''
+		return ret
+
+	def hash(self, h_type, size=0):
+		if h_type not in self.hash_types:
+			raise ValueError('Hash function not supported')
+		size = '' if size == 0 else size
+		ret = self._exec('ph %s %s%s' % (h_type, size, self._tmp_off))
+		self._tmp_off = ''
+		return ret
+
+	@property
+	def pwd(self):
+		return self._exec('pwd')
