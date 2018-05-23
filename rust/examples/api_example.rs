0a51a46accffe48a5d4a857d000ae8af0f07f66f
ZhangZhuoSJTU <zhangzhuo@sjtu.edu.cn>
2018-05-23 12:58:39 +0800

    fix test

--- rust/examples/api_example.rs
+++ rust/examples/api_example.rs
@@ -9,7 +9,10 @@ fn main() {
     let path = "/bin/ls";
     let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
     r2.init();
-    println!("{:#?}", r2.cc_info().expect("Failed to get function list."));
+    r2.analyze();
+    println!("{:#?}", r2.reg_info());
+    println!("{:#?}", r2.bin_info());
+    println!("{:#?}", r2.flag_info());
     println!("{:#?}", r2.fn_list());
     println!("{:#?}", r2.symbols());
     println!("{:#?}", r2.imports());
