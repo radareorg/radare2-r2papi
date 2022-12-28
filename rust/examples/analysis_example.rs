46feccd806c9504fce57221710185c53b408674a
rhl120 <rhl120@protonmail.com>
2022-12-28 16:36:09 +0100

    Fix test warns

--- rust/examples/analysis_example.rs
+++ rust/examples/analysis_example.rs
@@ -11,55 +11,55 @@ fn main() {
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_all();
+        r2.analyze_all().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_and_autoname();
+        r2.analyze_and_autoname().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_function_calls();
+        r2.analyze_function_calls().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_data_references();
+        r2.analyze_data_references().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_references_esil();
+        r2.analyze_references_esil().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_function_preludes();
+        r2.analyze_function_preludes().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_function_references();
+        r2.analyze_function_references().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_symbols();
+        r2.analyze_symbols().unwrap();
         println!("{:#?}", r2.fn_list());
     }
     {
         let path = "/bin/ls";
         let mut r2 = R2::new(Some(path)).expect("Failed to spawn r2");
-        r2.analyze_consecutive_functions();
+        r2.analyze_consecutive_functions().unwrap();
         println!("{:#?}", r2.fn_list());
     }
 }
