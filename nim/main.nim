63602bb5829de386107aaea574a105b2be375486
pancake <pancake@nopcode.org>
2023-04-16 10:06:49 +0200

    Add cmdj() for Nim

--- nim/main.nim
+++ nim/main.nim
@@ -1,8 +1,18 @@
 import r2papi
+import std/strformat
+import std/sugar
+import std/json
 
 var r = r2papi.R2Papi()
 r.speak ("Hello World")
 
+var bigint = 0xeeff_ffff_ffff_fff0
+
+var o = r.cmdj("ij");
+var fileName = o["core"]["file"].str
+echo(fileName)
+
+echo(fmt"{bigint:+x}")
 
 # echo("Hello World")
 # echo(r2cmd("x"))
--- nim/r2papi.nim
+++ nim/r2papi.nim
@@ -1,9 +1,14 @@
+import std/[json, options, sugar]
 
 proc r2cmd(arg: cstring): cstring {.importc}
 
 type R2Papi = object
 
+proc cmdj(self: R2Papi, cmd: string): JsonNode = 
+  var res = $r2cmd(cmd)
+  return parseJson(res)
+
 proc speak(self: R2Papi, msg: string) = 
   echo r2cmd("?E " & msg)
 
-export r2cmd, R2Papi, speak
+export r2cmd, cmdj, R2Papi, speak
