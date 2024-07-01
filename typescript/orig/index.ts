d7eb52f42b4756bbbbe3e77ee050c095db560d8d
pancake <pancake@nopcode.org>
2024-07-01 09:31:24 +0200

    Delete the orig directory. async will be src soon

--- typescript/orig/index.ts
+++
@@ -1,9 +0,0 @@
-import { R2Pipe } from "./r2pipe.js";
-
-export { R2Pipe } from "./r2pipe.js";
-export { R2AI } from "./ai.js";
-export { R, NativePointer, R2Papi, Reference, BasicBlock, ThreadClass } from "./r2papi.js";
-export { EsilNode, EsilParser } from "./esil.js";
-export { Base64, Base64Interface } from "./base64.js";
-
-export declare var r2: R2Pipe;
